5bcf722f58841fec22c4b750e91ff31fe8e0e919
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-03-09 22:03:40 +0100

    changed style of labels

--- pGroupeB03/src/view/WaitingScreen.java
+++ pGroupeB03/src/view/WaitingScreen.java
@@ -7,21 +7,13 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
 public class WaitingScreen extends BorderPane {
 	
-	private Label lblWelcome;
 	private Label lblPressToContinue;
 	
 	public WaitingScreen() {
 		
-		VBox vbCenter = new VBox();
-		vbCenter.setPadding(new Insets(5));
-		vbCenter.setSpacing(5);
-		vbCenter.getChildren().addAll(this.getLblWelcome());
-		vbCenter.setAlignment(Pos.CENTER);
-		this.setCenter(vbCenter);
 		
 		HBox hbBottom = new HBox();
 		hbBottom.setPadding(new Insets(5));
@@ -39,13 +31,6 @@ public class WaitingScreen extends BorderPane {
 		});
 	}
 	
-	public Label getLblWelcome() {
-		if(lblWelcome==null) {
-			lblWelcome = new Label("WELCOME IN THE GAME");
-			lblWelcome.setId("lblWelcome");
-		}
-		return lblWelcome;
-	}
 	public Label getLblPressToContinue() {
 		if(lblPressToContinue==null) {
 			lblPressToContinue = new Label("Click to continue");
