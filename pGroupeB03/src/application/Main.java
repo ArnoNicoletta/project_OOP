89e25dbfce680a5ba36964bbbf6539ee0cf0462d
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-03-29 18:53:04 +0200

    Cleaning

--- pGroupeB03/src/application/Main.java
+++ pGroupeB03/src/application/Main.java
@@ -4,9 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import view.AdminSettings;
-import view.GameController;
-import view.Highscore;
 import view.MainController;
 
 
@@ -14,13 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//AdminSettings root = new AdminSettings();
 			MainController root = new MainController();
 			Scene scene = new Scene(root,1024,576);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
-			//primaryStage.setFullScreen(true);
 			primaryStage.getIcons().addAll(new Image("file:./src/resources/images/logo.png"));
 			primaryStage.setTitle("FOUR THE WIN");
 			primaryStage.show();
@@ -31,12 +26,5 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
-		
-//		Game g = Game.getInstance();
-//		g.addDeck(new File("./src/resources/questions/deck_Sport.json"));
-//		g.addDeck(new File("./src/resources/questions/deck_Social Networks.json"));
-//		g.addDeck(new File("./src/resources/questions/deck_Academy Awards winners.json"));
-//		g.play();
-		
 	}
 }
