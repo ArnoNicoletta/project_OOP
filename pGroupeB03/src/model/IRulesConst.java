8dfa48478e8bca8c4e10495a92e4fb52d0bdd572
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-04-09 15:00:55 +0200

    Added Number of rounds

--- pGroupeB03/src/model/IRulesConst.java
+++ pGroupeB03/src/model/IRulesConst.java
@@ -2,8 +2,12 @@ package model;
 
 public interface IRulesConst {
 	
+	//Player rules
 	int MAX_CHAR = 16;
 	int MAX_PLAYER = 3;
+	//Question and Deck rules
 	int MIN_QUESTIONS = 10;
+	//Game rules
+	int NUMBER_ROUND = 1;
 	double ROUND_TIME_SECONDS = 45;
 }
