262b32d249ea79ddb87c00e1be81f50b8de7649d
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-04-09 16:35:58 +0200

    Added methods to change the value of rules

--- pGroupeB03/src/model/RulesConst.java
+++ pGroupeB03/src/model/RulesConst.java
@@ -1,19 +1,36 @@
 package model;
 
-import java.lang.reflect.Field;
+import exception.WrongRuleValueException;
 
-public class RulesConst {
+public abstract class RulesConst {
 	
 	//Player rules
-	public static int MAX_CHAR = 16;
-	public static int MAX_PLAYER = 3;
+	public final static int MAX_CHAR = 16;
+	public final static int MAX_PLAYER = 3;
 	//Question and Deck rules
-	public static int MIN_QUESTIONS = 10;
+	public final static int MIN_QUESTIONS = 10;
 	//Game rules
-	public static int NUMBER_ROUND = 1;
-	public static double ROUND_TIME_SECONDS = 45;
+	private static int number_round = 1;
+	private static double round_time_seconds = 45;
 	
-	public static void setMAX_CHAR(int mAX_CHAR) {
-		MAX_CHAR = mAX_CHAR;
+	
+	//Setters
+	public static void setNumber_round(int number_round) throws WrongRuleValueException {
+		if(number_round<1 || number_round>5) throw new WrongRuleValueException(""+number_round);
+		RulesConst.number_round = number_round;
 	}
+	public static void setRound_time_seconds(double round_time_seconds) throws WrongRuleValueException {
+		if(round_time_seconds<15 || round_time_seconds>90) throw new WrongRuleValueException(""+ round_time_seconds);
+		RulesConst.round_time_seconds = round_time_seconds;
+	}
+	
+	//Getters
+	public static int getNumber_round() {
+		return number_round;
+	}
+	public static double getRound_time_seconds() {
+		return round_time_seconds;
+	}
+	
+	
 }
