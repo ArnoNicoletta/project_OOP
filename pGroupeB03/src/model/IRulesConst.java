f5e6a91ec207ee9ce8911738f7d2a7370e5506f5
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-04-09 15:50:10 +0200

    Added changeRule + changed into class

--- pGroupeB03/src/model/IRulesConst.java
+++ pGroupeB03/src/model/IRulesConst.java
@@ -1,13 +1,24 @@
 package model;
 
-public interface IRulesConst {
+import java.lang.reflect.Field;
+
+public class IRulesConst {
 	
 	//Player rules
-	int MAX_CHAR = 16;
-	int MAX_PLAYER = 3;
+	public static int MAX_CHAR = 16;
+	public static int MAX_PLAYER = 3;
 	//Question and Deck rules
-	int MIN_QUESTIONS = 10;
+	public static int MIN_QUESTIONS = 10;
 	//Game rules
-	int NUMBER_ROUND = 1;
-	double ROUND_TIME_SECONDS = 45;
+	public static int NUMBER_ROUND = 1;
+	public static double ROUND_TIME_SECONDS = 45;
+	
+	public static void changeRule(String s, double newValue) {
+		try {
+			Field f = IRulesConst.class.getField(s);
+			f.set(f.getClass(), newValue);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
