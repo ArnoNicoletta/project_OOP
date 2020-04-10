fa42b5e70399f76ec1237387a56216c0af54e359
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-04-10 10:45:01 +0200

    Added pause window

--- pGroupeB03/src/view/MsgBox.java
+++ pGroupeB03/src/view/MsgBox.java
@@ -7,8 +7,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MsgBox{
 	
@@ -17,6 +20,7 @@ public class MsgBox{
 	public static boolean displayYesNO(String title, String message) {
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
+		window.initStyle(StageStyle.TRANSPARENT);
 		window.setTitle(title);
 		window.setResizable(false);
 		window.setOnCloseRequest(e -> answer = false);
@@ -54,6 +58,7 @@ public class MsgBox{
 	public static void dispalyOk(String title, String message) {
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
+		window.initStyle(StageStyle.TRANSPARENT);
 		window.setTitle(title);
 		window.setResizable(false);
 		
@@ -80,6 +85,7 @@ public class MsgBox{
 	public static void dispalyException(Exception e) {
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
+		window.initStyle(StageStyle.TRANSPARENT);
 		window.setTitle(e.getClass().getSimpleName().substring(0, e.getClass().getSimpleName().length()-9));
 		window.setResizable(false);
 		
@@ -103,4 +109,55 @@ public class MsgBox{
 		window.showAndWait();
 	}
 	
+	public static boolean displayPause(String player, String theme) {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.initStyle(StageStyle.TRANSPARENT);
+		window.setTitle("Waiting");
+		window.setResizable(false);
+		window.setOnCloseRequest(e -> answer = false);
+		
+		Label lblPause = new Label("GAME WAITING");
+		lblPause.setTextFill(Color.DARKGRAY);
+		lblPause.setAlignment(Pos.CENTER);
+		lblPause.setPadding(new Insets(5));
+		
+		Label lblInfos = new Label("Player : " + player 
+				+ "\nTheme : " + theme);
+		lblInfos.setAlignment(Pos.CENTER);
+		lblInfos.setPadding(new Insets(5));
+		lblInfos.setWrapText(true);
+		
+		Button btnExit = new Button("EXIT");
+		btnExit.setPrefSize(IGraphicConst.WIDTH_BUTTON/2, IGraphicConst.HEIGHT_BUTTON);
+		btnExit.setOnAction(e -> {
+			answer = false;
+			window.close();
+		});
+		
+		Button btnPlay = new Button("PLAY");
+		btnPlay.setPrefSize(IGraphicConst.WIDTH_BUTTON, IGraphicConst.HEIGHT_BUTTON);
+		btnPlay.setOnAction(e -> {
+			answer = true;
+			window.close();
+		});
+		
+		BorderPane bp = new BorderPane(lblInfos);
+		HBox hbTop = new HBox();
+		hbTop.setAlignment(Pos.CENTER);
+		hbTop.getChildren().add(lblPause);
+		bp.setTop(hbTop);
+		HBox hbBottom = new HBox(5);
+		hbBottom.setAlignment(Pos.CENTER_RIGHT);
+		hbBottom.setPadding(new Insets(5));
+		hbBottom.getChildren().addAll(btnExit, btnPlay);
+		bp.setBottom(hbBottom);
+		
+		Scene scene = new Scene(bp, 512, 288);
+		window.setScene(scene);
+		
+		window.showAndWait();
+		
+		return answer;
+	}
 }
