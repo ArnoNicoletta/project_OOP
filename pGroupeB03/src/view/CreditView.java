176327f9cf68ed2c225ac94b659452a539ce4ffa
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-05-01 23:43:10 +0200

    ui done

--- pGroupeB03/src/view/CreditView.java
+++ pGroupeB03/src/view/CreditView.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -21,11 +22,22 @@ public class CreditView extends BorderPane {
 				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, 
 				BackgroundPosition.CENTER,  
 				new BackgroundSize(IGraphicConst.WIDTH_BACKGROUND, IGraphicConst.HEIGHT_BACKGROUND, false, false, false, false))));
+		this.setCenter(getLblCredit());
 	}
 	
-	public Label getLabelCredit() {
+	public Label getLblCredit() {
 		if(lblCredit==null) {
 			lblCredit = new Label();
+			IGraphicConst.styleBiggerLabel(lblCredit);
+			lblCredit.setPrefSize(IGraphicConst.WIDTH_BACKGROUND, IGraphicConst.HEIGHT_BACKGROUND);
+			lblCredit.setPadding(new Insets(25));
+			lblCredit.setWrapText(true);
+			lblCredit.setText("\n\nThis game was produced as part of the Project course given by Mr. ALTARES MENENDEZ "
+					+ "in the second year of Bachelor in Computer Science at Helha in Mons.\r\n" + 
+					"\nThe group that produced it is made up of:\r\n" + 
+					"	Arno NICOLETTA\r\n" + 
+					"	Rayan TAHRI\r\n" + 
+					"	Loïc LÉCOLIER");
 		}
 		return lblCredit;
 	}
