31f250c514a5c11e92d3b3ac02ceb7cd5fb4a5fd
Arno NICOLETTA <nicoletta.arno@gmail.com>
2020-05-02 17:46:15 +0200

    removed setResizable() and update setOnClose

--- pGroupeB03/src/application/Main.java
+++ pGroupeB03/src/application/Main.java
@@ -1,11 +1,9 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import view.IGraphicConst;
 import view.MainView;
 import view.MsgBox;
@@ -20,7 +18,6 @@ public class Main extends Application {
 			Scene scene = new Scene(root,1024,576);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-//			primaryStage.setResizable(false);
 			primaryStage.setMinWidth(1030);
 			primaryStage.setMinHeight(580);
 			primaryStage.setMaxWidth(1057);
@@ -29,13 +26,10 @@ public class Main extends Application {
 			primaryStage.setTitle("FOUR THE WIN");
 			primaryStage.sizeToScene();
 			primaryStage.show();
-			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-				@Override
-				public void handle(WindowEvent event) {
-					event.consume();
-					if(MsgBox.displayYesNO("Exit", "Are you sure you want to exit?")) {
-						primaryStage.close();
-					}
+			primaryStage.setOnCloseRequest(event -> {
+				event.consume();
+				if(MsgBox.displayYesNO("Exit", "Are you sure you want to exit?")) {
+					primaryStage.close();
 				}
 			});
 		} catch(Exception e) {
